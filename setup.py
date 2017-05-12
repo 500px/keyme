77bbf84f336f56eda5df3e50e154f87070dffd2a
Steve Houser <shouser@gmail.com>
2017-05-12 17:49:45 -0400

    * cleaned up some bugs surrounding missing files and directories
    Signed-off-by: Steve Houser <shouser@gmail.com>

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import find_packages, setup
 dependencies = ['click', 'boto3', 'bs4', 'beautifulsoup', 'requests', 'py', 'pyyaml']
 
 setup(name='keyme',
-      version='0.6.1',
+      version='0.6.2',
       description='Google SAML STS login library',
       url='http://github.com/wheniwork/keyme',
       author='Richard Genthner',
